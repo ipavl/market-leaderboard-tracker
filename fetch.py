757445f248f0ef26866369d49406f09ffd493fd7
ipavl <ipavl@users.sourceforge.net>
2015-06-22 18:55:08 -0400

    Store data in SQLite database

--- fetch.py
+++ fetch.py
@@ -4,6 +4,8 @@ from lxml import html
 
 import re
 import requests
+import sqlite3
+import time
 
 page = requests.get('https://play.google.com/store/apps/collection/topselling_free')
 tree = html.fromstring(page.text)
@@ -15,7 +17,24 @@ apps = tree.xpath('//h2/a[@class="title"]/text()')
 apps = [line.strip() for line in apps]
 apps = filter(None, apps)
 
-for app in apps:
+# Database setup
+conn = sqlite3.connect('data.sqlite')
+c = conn.cursor()
+
+c.execute('''CREATE TABLE IF NOT EXISTS leaderboard
+        (name TEXT, date TEXT, rank INTEGER)''')
+
+data = []
+date = time.strftime('%Y-%m-%d')
+
+for i, app in enumerate(apps):
     # Remove numeric rank identifier portion
     app = re.sub('^[0-9]*\.\ *', '', app)
-    print (app)
+
+    data.append((app, date, i + 1))
+
+# Store the data in the database
+c.executemany('INSERT INTO leaderboard VALUES (?, ?, ?)', data)
+
+conn.commit()
+conn.close()
