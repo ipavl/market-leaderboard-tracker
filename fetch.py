cede9e3e4dd13db9d467098f5b0d2b8a51a32586
ipavl <ipavl@users.sourceforge.net>
2015-06-22 18:30:11 -0400

    Clean up app name by removing ranking substring

--- fetch.py
+++ fetch.py
@@ -2,6 +2,7 @@
 
 from lxml import html
 
+import re
 import requests
 
 page = requests.get('https://play.google.com/store/apps/collection/topselling_free')
@@ -15,4 +16,6 @@ apps = [line.strip() for line in apps]
 apps = filter(None, apps)
 
 for app in apps:
+    # Remove numeric rank identifier portion
+    app = re.sub('^[0-9]*\.\ *', '', app)
     print (app)
